7cda0e674b3b34564bd0c827d2df432ce3aa8524
tschumann <schumann.tom@gmail.com>
2021-12-31 15:49:53 +1000

    Moved .pdf logic into separate class.

--- src/main/kotlin/ktext/Main.kt
+++ src/main/kotlin/ktext/Main.kt
@@ -1,7 +1,5 @@
 package ktext
 
-import com.itextpdf.kernel.pdf.*
-import jtext.Util
 import java.io.File
 import kotlin.jvm.JvmStatic
 
@@ -16,31 +14,18 @@ class Main {
                 return
             }
 
-            val document: PdfDocument = PdfDocument(PdfReader(args[0]), PdfWriter(Util.getOutputName(args[0])))
+            val pdf: PdfFile = PdfFile(args[0])
 
             when (args[1]) {
                 "--rotate" -> {
-                    val numberOfPages: Int = document.numberOfPages
-                    println("Rotating $numberOfPages pages in ${args[0]} 90 degrees clockwise")
-
-                    for (p in 1..numberOfPages) {
-                        val page: PdfPage = document.getPage(p)
-                        val rotation: Int = page.rotation
-
-                        page.rotation = (rotation + 90) % 360
-                    }
+                    pdf.rotate()
                 }
                 "--info" -> {
-                    val info: PdfDocumentInfo = document.documentInfo
-                    println("Author: " + (info.author ?: ""))
-                    println("Subject: " + (info.subject ?: ""))
-                    println("Keywords: " + (info.keywords ?: ""))
-                    println("Creator: " + (info.creator ?: ""))
-                    println("Producer: " + (info.producer ?: ""))
+                    pdf.printInfo()
                 }
             }
 
-            document.close()
+            pdf.close()
         }
     }
 }
---
+++ src/main/kotlin/ktext/PdfFile.kt
@@ -0,0 +1,35 @@
+package ktext
+
+import com.itextpdf.kernel.pdf.*
+import jtext.Util
+
+class PdfFile(fileName: String) {
+
+    private val fileName: String = fileName
+    private val document: PdfDocument = PdfDocument(PdfReader(fileName), PdfWriter(Util.getOutputName(fileName)))
+
+    fun rotate() {
+        val numberOfPages: Int = document.numberOfPages
+        println("Rotating $numberOfPages pages in ${fileName} 90 degrees clockwise")
+
+        for (p in 1..numberOfPages) {
+            val page: PdfPage = document.getPage(p)
+            val rotation: Int = page.rotation
+
+            page.rotation = (rotation + 90) % 360
+        }
+    }
+
+    fun printInfo() {
+        val info: PdfDocumentInfo = document.documentInfo
+        println("Author: " + (info.author ?: ""))
+        println("Subject: " + (info.subject ?: ""))
+        println("Keywords: " + (info.keywords ?: ""))
+        println("Creator: " + (info.creator ?: ""))
+        println("Producer: " + (info.producer ?: ""))
+    }
+
+    fun close() {
+        document.close()
+    }
+}
